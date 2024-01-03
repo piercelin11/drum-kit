cd6836750e3b4442fa45beeef928291b6b6cb02c
piercelin11 <135949754+piercelin11@users.noreply.github.com>
2024-01-03 21:18:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,72 @@
+function playSound(key){
+
+    switch(key){
+        case "a" :
+            var clap = new Audio("sounds/clap.wav");
+            clap.play();
+        break;
+
+        case "s" :
+            var hihat = new Audio("sounds/hihat.wav");
+            hihat.play();
+        break;
+
+        case "d" :
+            var kick = new Audio("sounds/kick.wav");
+            kick.play();
+        break;
+
+        case "f" :
+            var openhat = new Audio("sounds/openhat.wav");
+            openhat.play();
+        break;
+
+        case "g" :
+            var boom = new Audio("sounds/boom.wav");
+            boom.play();
+        break;
+
+        case "h" :
+            var ride = new Audio("sounds/ride.wav");
+            ride.play();
+        break;
+
+        case "j" :
+            var snare = new Audio("sounds/snare.wav");
+            snare.play();
+        break;
+
+        case "k" :
+            var tom = new Audio("sounds/tom.wav");
+            tom.play();
+        break;
+
+        case "l" :
+            var tink = new Audio("sounds/tink.wav");
+            tink.play();
+        break;
+    }
+}
+
+
 $(document).keydown(function(e){
-    let keyCode = e.keyCode;
-    let audio = $("audio[data-key='" + keyCode + "']");
+    let keyCode = e.key;
     let keyPressed = $(".key[data-key='" + keyCode + "']");
 
-    if(audio.length > 0 && keyPressed.length > 0){
+    if(keyPressed.length > 0){
         keyPressed.addClass("playing");
-        audio[0].currentTime = 0;
-        audio[0].play();
+        playSound(keyCode);
     }
 
 });
 
 $(".key").click(function(){
     let keyCode = $(this).attr("data-key");
-    let audio = $("audio[data-key='" + keyCode + "']");
     let keyPressed = $(".key[data-key='" + keyCode + "']");
 
     if(audio.length > 0 && keyPressed.length > 0){
         keyPressed.addClass("playing");
-        audio[0].currentTime = 0;
-        audio[0].play();
+        playSound(keyCode);
         setTimeout(function(){
             keyPressed.removeClass("playing");
         }, 100);
@@ -28,10 +75,11 @@ $(".key").click(function(){
 });
 
 $(document).keyup(function(e){
-    let keyCode = e.keyCode;
+    let keyCode = e.key;
     let keyPressed = $(".key[data-key='" + keyCode + "']");
 
     if(keyPressed.length > 0){
         keyPressed.removeClass("playing");
     }
 });
+
